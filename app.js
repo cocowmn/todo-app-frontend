eb16daa7df5e5970f9eb6e9841d1722ea6c99eef
Coal Cooper <cocowmn@gmail.com>
2023-02-20 19:11:46 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+/** UI OBJECTIVES:
+ *    1: Create a list, Display it on screen
+ *      1a: Display List Title
+ *      1b: Create a To-Do Item inside of a List
+ *      1c: Display Due Date when Available
+ *    2: Update a list when a new item is added
+ *      2a: Handle Form Input
+ */
+
+/** API OBJECTIVES:
+ *    1: Get All Lists
+ *    2: Create A New List
+ *    3: Create A To-Do Item
+ * 
+ *    x: add error handling and logging for failure states
+ */
+
+
+console.log('Hello!')
