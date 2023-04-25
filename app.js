fe69ac48cee2cee0497c28727ba8ff067579a0a3
Coal Cooper <cocowmn@gmail.com>
2023-04-25 14:02:38 -0400

    class progress

--- app.js
+++ app.js
@@ -15,5 +15,42 @@
  *    x: add error handling and logging for failure states
  */
 
+const allListsResponse = await fetch('http://localhost:8080/lists/all', {
+  method: 'GET'
+})
+const allLists = await allListsResponse.json()
 
-console.log('Hello!')
+/*
+  W3Schools [HTML <template> Tag]
+  https://www.w3schools.com/tags/tag_template.asp
+  ---------------------------
+  function showContent() {
+    var temp = document.getElementsByTagName("template")[0];
+    var clon = temp.content.cloneNode(true);
+    document.body.appendChild(clon);
+  }
+*/
+
+function displayList(list) {
+  const template = document.querySelector('#todo-list-template')
+  const templateCopy = template.content.cloneNode(true)
+
+  const titleContainer = templateCopy.querySelector('.todo-list-title')
+  titleContainer.innerText = list.title
+
+  const itemsContainer = templateCopy.querySelector('.todo-list-items')
+  for(let todoItem of list.toDoItems) {
+    console.log(todoItem)
+    const itemElement = document.createElement('li')
+    itemElement.innerText = todoItem.content
+    itemsContainer.appendChild(itemElement)
+  }
+  
+  document.body.appendChild(templateCopy)
+}
+
+console.log(allLists)
+
+for(let list of allLists) {
+  displayList(list)
+}
