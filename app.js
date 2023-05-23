cbbc3af7a0cf13d560c263f2506b2bb8ab9c7202
Coal Cooper <cocowmn@gmail.com>
2023-05-23 17:33:02 -0400

    add index.html and app.js
    add base functionality to query backend for all lists and render this data to the browser window

--- app.js
+++ app.js
@@ -15,5 +15,45 @@
  *    x: add error handling and logging for failure states
  */
 
+console.log('app.js loaded')
 
-console.log('Hello!')
+const allListsResponse = await fetch(
+  'http://localhost:8080/lists/all',
+  {
+    method: 'GET'
+  })
+const allLists = await allListsResponse.json()
+
+function displayList( list ) {
+  const listContainer = document.querySelector( '#list-container' )
+  const listTemplate = document.querySelector('#template-list')
+  const newList = listTemplate.content.cloneNode(true)
+
+  const listTitleElement = newList.querySelector( '.todo-list-title' )
+  const listItemsElement = newList.querySelector( '.todo-items' )
+
+  listTitleElement.innerText = list.title
+
+  list.toDoItems.forEach(item => {
+    const newItemElement = document.createElement( 'li' )
+    newItemElement.innerText = item.content
+
+    listItemsElement.appendChild( newItemElement )
+  })
+
+  //
+  // This for loop is an alternate way of writing the above .forEach loop
+  //
+  // for(let index = 0; index < list.toDoItems.length; index++) {
+  //   const item = list.toDoItems[ index ]
+
+  //   const newItemElement = document.createElement( 'li' )
+  //   newItemElement.innerText = item.content
+
+  //   listItemsElement.appendChild( newItemElement )
+  // }
+  
+  listContainer.appendChild( newList )
+}
+
+allLists.forEach( list => displayList(list) )
